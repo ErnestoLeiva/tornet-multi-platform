70e6787803a51b321832b2e1b0952d75754cbce7
Ernesto Leiva <eleiv009@fiu.edu>
2025-05-20 07:18:59 -0400

    chore: update LICENSE and setup.py for project rebranding and author information

--- setup.py
+++ setup.py
@@ -4,28 +4,32 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='tornet',
+    name='tornet-multi-platform',
     version='2.1.0',
     packages=find_packages(),
     install_requires=[
         'requests',
         'requests[socks]',
+        'pysocks'
     ],
     entry_points={
         'console_scripts': [
             'tornet=tornet.tornet:main',
         ],
     },
-    author='Ayad Seghiri',
-    author_email='seghiri.ayad@univ-khenchela.dz',
-    description='TorNet - Automate IP address changes using Tor',
+    author='Ernesto Leiva',
+    author_email='contact@ernestoleiva.com',
+    description='TorNet - Cross-platform IP rotation using Tor with logging and automation',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/ayadseghairi/tornet',
+    url='https://github.com/ErnestoLeiva/tornet-multi-platform',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Topic :: Internet :: Proxy Servers',
+        'Topic :: Utilities'
     ],
     python_requires='>=3.6',
 )
